9bd030bb730860b409255d8c66791abee92a368d
unknown <dragostinj@gmail.com>
2020-09-16 12:31:02 +0100

    connected to db and more

---
+++ server/index.js
@@ -0,0 +1,28 @@
+//Starting point of the App
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+
+const mongoose = require('mongoose');
+
+//DB Setup
+const uri = 'mongodb://localhost:27017/db';
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+const connection = mongoose.connection
+
+connection.once('open', function() {
+    console.log('MongoDB database connection established successfully');
+})
+//App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+//Server Setup
+const port = process.env.PORT || 4000;
+const server = http.createServer(app);
+server.listen(port);
+console.log(`server listening on ${port}`);
---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Define Model
+const userSchema = new Schema({
+  email: {type: String, unique: true, lowercase: true },
+  password: String,
+});
+
+
+
+//Create Model Class
+const ModelClass = mongoose.model("user", userSchema);
+
+//Export the Model
+module.exports = ModelClass;
