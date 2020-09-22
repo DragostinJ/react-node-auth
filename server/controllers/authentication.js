10be99b1b6e4122ea334a8636f7e500926de68b1
unknown <dragostinj@gmail.com>
2020-09-22 13:23:34 +0100

    Sign in and Signup set and protected

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,47 +1,46 @@
-const User = require("../models/user");
-const jwt = require("jwt-simple");
-const config = require("../config");
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
-exports.signup = function (req, res, next) {
-  const email = req.body.email;
 
+exports.signin = function(req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
   const password = req.body.password;
 
-  // TODO: more validation on email and password
-
   if (!email || !password) {
-    return res
-      .status(422)
-      .send({ error: "You must provide a password and email" });
+    return res.status(422).send({ error: 'You must provide email and password'});
   }
-  // See  if the user with the given email exist
-  User.findOne({ email: email }, function (err, existingUser) {
-    if (err) {
-      return next(err);
-    }
 
-    // if a user with this email exist, return error
+  // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
+    // If a user with email does exist, return an error
     if (existingUser) {
-      return res.status(422).send({ error: "Email is in use" });
-      //422 bad data
+      return res.status(422).send({ error: 'Email is in use' });
     }
-    // if user with email does not exist create and save user record
+
+    // If a user with email does NOT exist, create and save user record
     const user = new User({
       email: email,
-      password: password,
+      password: password
     });
 
-    user.save(function (err) {
-      if (err) {
-        return next(err);
-      }
-      //respond to request indicatin the user was created
-      res.json({token: tokenForUser(user) });
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Repond to request indicating the user was created
+      res.json({ token: tokenForUser(user) });
     });
   });
-};
+}
