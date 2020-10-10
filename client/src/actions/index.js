f223a76c1ce0597a09620f9cd63143c2b4a6d96e
unknown <dragostinj@gmail.com>
2020-10-10 23:39:50 +0100

    Finished log in and log out

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 import { AUTH_USER, AUTH_ERROR } from "./types";
 
+
+
 export const signup = (formProps, callback) => async dispatch => {
         try {
       const response = await  axios.post('http://localhost:4000/signup', formProps)
 
       dispatch({ type: AUTH_USER, payload: response.data.token })
+      localStorage.setItem('token', response.data.token)
       callback();
 } catch (e){
         //
@@ -13,4 +16,27 @@ export const signup = (formProps, callback) => async dispatch => {
                 type: AUTH_ERROR, payload: 'Email in use'
         })
 }
+}
+
+export const signin = (formProps, callback) => async dispatch => {
+        try {
+      const response = await  axios.post('http://localhost:4000/signin', formProps)
+
+      dispatch({ type: AUTH_USER, payload: response.data.token })
+      localStorage.setItem('token', response.data.token)
+      callback();
+} catch (e){
+        //
+        dispatch({
+                type: AUTH_ERROR, payload: 'Invalid login credentials'
+        })
+}
+}
+export const signout = ()=> {
+        localStorage.removeItem('token')
+
+        return{
+                type: AUTH_USER,
+                payload: ''
+        }
 }
